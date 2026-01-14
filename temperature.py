625abddbb70a5f9bfa438742032798dee14b4652
Swapna.Rangain <01fe24ica029@kletech.ac.in>
2026-01-14 22:41:18 +0530

    first commit

---
+++ temperature.py
@@ -0,0 +1,7 @@
+def celcius_to_fahrenheit(celcius):
+    return (celcius*9/5)+32
+
+if__name__="__main__"
+celcius=float(input("enter temperture in celcius:"))
+fahrenheit=celcius_to_fahrenheit(celcius)
+print(f"temperature in faherenheit",{fahrenheit})
