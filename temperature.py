6f633bba7b90bc5df852f08f1e57b860957f26ac
swapna619 <swapnarangain619@gmail.com>
2026-01-14 22:58:37 +0530

    temperature.py

--- temperature.py
+++ temperature.py
@@ -1,7 +1,12 @@
-def celcius_to_fahrenheit(celcius):
-    return (celcius*9/5)+32
+import sys
 
-if__name__="__main__"
-celcius=float(input("enter temperture in celcius:"))
-fahrenheit=celcius_to_fahrenheit(celcius)
-print(f"temperature in faherenheit",{fahrenheit})
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python temperature.py <celsius>")
+    else:
+        celsius = float(sys.argv[1])
+        fahrenheit = celsius_to_fahrenheit(celsius)
+        print(f"Temperature in Fahrenheit: {fahrenheit}")
