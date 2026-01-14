205cf3af6fb4f4b2e381a62ec6886b71b26530cf
swapna619 <swapnarangain619@gmail.com>
2026-01-14 23:20:04 +0530

    temperature.py

--- temperature.py
+++ temperature.py
@@ -1,3 +1,5 @@
+import sys
+
 def attendance_status(classes_held, classes_attended):
     percentage = (classes_attended / classes_held) * 100
     if percentage >= 75:
@@ -5,12 +7,21 @@ def attendance_status(classes_held, classes_attended):
     else:
         return percentage, "Not Eligible"
 
+def main():
+    # Jenkins / Command-line execution
+    if len(sys.argv) == 3:
+        classes_held = int(sys.argv[1])
+        classes_attended = int(sys.argv[2])
 
-if __name__ == "__main__":
-    classes_held = int(input("Enter number of classes held: "))
-    classes_attended = int(input("Enter number of classes attended: "))
+    # Local interactive execution
+    else:
+        classes_held = int(input("Enter number of classes held: "))
+        classes_attended = int(input("Enter number of classes attended: "))
 
     percentage, status = attendance_status(classes_held, classes_attended)
 
     print(f"Attendance Percentage: {percentage:.2f}%")
-    print(status)
+    print(f"Eligibility: {status}")
+
+if __name__ == "__main__":
+    main()
